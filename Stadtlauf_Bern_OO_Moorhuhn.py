03ff7e3b6a967f53a05611168986ff3958f14dfd
Peter Christen <peter.christen@cssgmbh.ch>
2026-03-29 22:35:33 +0200

    Mordernisiert

--- Stadtlauf_Bern_OO_Moorhuhn.py
+++ Stadtlauf_Bern_OO_Moorhuhn.py
@@ -1,135 +1,235 @@
 ##############################################
 #
-# Name: Stadtlauf_Bern_OO.py
+# Name: Stadtlauf_Bern_OO_Moorhuhn.py
 #
 # Author: Peter Christen
 #
-# Version: 1.0
+# Version: 2.0
 #
-# Date: 10.09.2022
+# Date: 10.09.2022 V1.0
+#       29.03.2026 V2.0 Mit KI überarbeitet
 #
-# Purpose: Marsi läuft in der Stadt Bern herum
+# Purpose: Marsi läuft in der Stadt Bern herum, mehrere Moorhühner fliegen umher
 #
 ##############################################
 
-#Module
+# Module
 import time
 import random
 import stadtlauf_bern_oo_moorhuhn_modul as sf
 import stadtlauf_bern_sehenswuerdigkeiten_inventar as sbi
 import stadtlauf_bern_wegpunkte as wp
 
-#Variablen
-run=True
-slower=0.005   #Je höher die Zahl, umso langsamer läuft die Figur
-wege={}
+# Konfiguration
+SLOWER = 0.005              # Je höher die Zahl, umso langsamer läuft die Figur
+ANZAHL_MOORHUEHNER = 3      # Anzahl Moorhühner
+MOORHUHN_INTERVALL = 50     # Ticks bis zum nächsten Start eines Moorhuhns
 
-moorhuhn={"Status":"Stop","x":1000,"y":20,"Seite":"Stop",'intervall':50,'counter':0}
-marsmensch={"Status":"Go","x":20,"y":150,"Seite":"Stop"}
+# Globale Variablen
+run = True
+wege = {}
 
-#Figur als Objekt initialisieren
-marsi=sf.Figur('Marsi')
-moori=sf.Figur('Moori')
+# Hilfsfunktionen
 
-#Funktion
-def check_text(x,y):
-   '''Prüft ob eine Sehenswürdigkeit in der Nähe ist'''
 
-   sehenswurdigkeit=''
-   for s in sbi.sehensw:
-      if x in sbi.pos[s][0] and y in sbi.pos[s][1]:
-         sehenswurdigkeit=s
-   return sehenswurdigkeit,x,y-15
+def kollidiert_rect(a, b, width_a=40, height_a=60, width_b=40, height_b=60):
+    """Einfache Rechteck-Kollision zwischen zwei Dicts mit x,y."""
+    ax1, ay1 = a['x'], a['y']
+    ax2, ay2 = ax1 + width_a, ay1 + height_a
+    bx1, by1 = b['x'], b['y']
+    bx2, by2 = bx1 + width_b, by1 + height_b
+
+    if ax2 < bx1 or ax1 > bx2 or ay2 < by1 or ay1 > by2:
+        return False
+    return True
+
+
+def check_text(x, y):
+    """Prüft ob eine Sehenswürdigkeit in der Nähe ist."""
+    sehenswurdigkeit = ''
+    for s in sbi.sehensw:
+        if x in sbi.pos[s][0] and y in sbi.pos[s][1]:
+            sehenswurdigkeit = s
+    return sehenswurdigkeit, x, y - 15
+
 
 def walk_generator(weg):
-   '''Schrittgenerator'''
+    """Schrittgenerator."""
+    schritte = len(weg)
+    s = 0
+    while s < schritte:
+        yield s
+        s += 1
 
-   schritte=len(weg)
-   s=0
-
-   while s<=schritte:
-      yield s
-      s+=1
 
 def wege_analysieren():
-   '''Die möglichen Wege an einer Position aufführen'''
+    """Die möglichen Wege an einer Position aufführen."""
+    for k in wp.wegpunkt.keys():
+        l = k.split('-')
+        if l[0] in wege.keys():
+            wege[l[0]].append(l)
+        else:
+            wege[l[0]] = [l]
 
-   for k in wp.wegpunkt.keys():
-      l=k.split('-')
-      if l[0] in wege.keys():
-         wege[l[0]].append(l)
-      else:
-         wege[l[0]]=[l]
 
 def neues_ziel_definieren(start):
-   '''Ziel ab Startpunkt definieren'''
+    """Ziel ab Startpunkt definieren."""
+    zufall = random.randint(0, len(wege[start]) - 1)
+    start, ende = wege[start][zufall]
+    strecke = start + "-" + ende
+    return strecke, ende
 
-   zufall=random.randint(0,3)
-   start,ende=wege[start][zufall]
-   strecke=start+"-"+ende
 
-   return strecke,ende   
-
+# Wege vorbereiten
 wege_analysieren()
 
-#Erste Strecke definieren
-strecke,ende=neues_ziel_definieren('Bahnhofplatz')
-schritt=walk_generator(wp.wegpunkt[strecke])
+# Erste Strecke definieren
+strecke, ende = neues_ziel_definieren('Bahnhofplatz')
+schritt = walk_generator(wp.wegpunkt[strecke])
 
+# Figuren als Objekte initialisieren
+# Marsmenschen als Liste (aktuell nur einer)
+marsmenschen = []
+marsi = sf.Figur('Marsi')
+marsmenschen.append({
+    "name": "Marsi0",
+    "obj": marsi,
+    "Status": "Go",
+    "x": 20,
+    "y": 150,
+    "Seite": "Stop"
+})
+
+# Mehrere Moorhühner
+moorhuehner = []
+for i in range(ANZAHL_MOORHUEHNER):
+    moori = sf.Figur(f"Moori{i}")
+    moorhuehner.append({
+        "name": f"Moorhuhn{i}",
+        "obj": moori,
+        "Status": "Stop",
+        "x": 1000,
+        "y": 20,
+        "Seite": "Stop",
+        "intervall": MOORHUHN_INTERVALL,
+        "counter": random.randint(0, MOORHUHN_INTERVALL),
+        "richtung": "right"
+    })
+
+# Sonne als eigene "Figur" (für Kollision)
+sonne_dict = {"x": 0, "y": 0}
+
+
+# Hauptschleife
 while run:
     sf.clock.tick(27)
-    time.sleep(slower)  #Laufgeschwindigkeit reduzieren
+    time.sleep(SLOWER)  # Laufgeschwindigkeit reduzieren
 
-    if moorhuhn['counter'] > moorhuhn['intervall']:
-       moorhuhn['y']=random.randint(10,300)
-       dire=random.choice(("right","left"))
-       if dire=="right": moorhuhn['x']=0
-       else: moorhuhn['x']=1000
-       moorhuhn['counter']=0
-       moorhuhn['Status']="Go"
-    elif moorhuhn['Status']=="Go":
-          #In welche Richtung soll es gehen
-          if dire=="right":
-             moorhuhn['x'],moorhuhn['Seite']=moori.go_right(moorhuhn['x'],3)
-          else:
-             moorhuhn['x'],moorhuhn['Seite']=moori.go_left(moorhuhn['x'],3)
-          #Ist das Moorhuhn in der Nähe der Sonne
-          if moorhuhn['x'] <= xs+10 and moorhuhn['x'] >= xs-10 and moorhuhn['y'] <= ys+15 and moorhuhn['y'] >= ys-60: 
-             print(f"Mohrhuhn tot, xh:{moorhuhn['x']},xs:{xs},yh:{moorhuhn['y']},ys:{ys}")
-             moorhuhn['Status']='tot'
-          #Ist das Moorhuhn in der Nähe von Marsi
-          if moorhuhn['x'] <= marsmensch['x']+10 and moorhuhn['x'] >= marsmensch['x']-10 and moorhuhn['y'] <= marsmensch['y']+15 and moorhuhn['y'] >= marsmensch['y']-60: 
-             print(f"Mohrhuhn gefressen, xh:{moorhuhn['x']},xm:{moorhuhn['x']},yh:{moorhuhn['y']},ym:{marsmensch['y']}")
-             moorhuhn['Status']='Stop'
-          #Ist das Moorhuhn am Bild Ende
-          if moorhuhn['x']<0 or moorhuhn['x']>999:
-             moorhuhn['counter']=0
-             moorhuhn['Status']='Stop'
-    elif moorhuhn['Status']=="tot":
-          moorhuhn['y']=moori.go_down(moorhuhn['y'],2)
-          if moorhuhn['y'] >= 340:
-             moorhuhn['counter']=0
-             moorhuhn['Status']='Stop'
-    else:
-       moorhuhn['counter']+=1
-
-    #Marsi läuft seine Strecke ab
+    # Marsmenschen bewegen sich entlang der Strecke (für Beispiel nur marsmenschen[0])
+    akt_mars = marsmenschen[0]
     try:
-       schr=next(schritt)
-       gx,gy,links,rechts=wp.wegpunkt[strecke][schr]
-       if links:
-          marsmensch['x'],marsmensch['y'],marsmensch['Seite']=marsi.go_walk_left(gx,gy)
-       else:
-          marsmensch['x'],marsmensch['y'],marsmensch['Seite']=marsi.go_walk_right(gx,gy)
-    except:
-       strecke,ende=neues_ziel_definieren(ende)
-       schritt=walk_generator(wp.wegpunkt[strecke])
-       print(f"Laufe Strecke {strecke}")
+        schr = next(schritt)
+        gx, gy, links, rechts = wp.wegpunkt[strecke][schr]
+        if links:
+            akt_mars['x'], akt_mars['y'], akt_mars['Seite'] = akt_mars['obj'].go_walk_left(
+                gx, gy)
+        else:
+            akt_mars['x'], akt_mars['y'], akt_mars['Seite'] = akt_mars['obj'].go_walk_right(
+                gx, gy)
+    except StopIteration:
+        strecke, ende = neues_ziel_definieren(ende)
+        schritt = walk_generator(wp.wegpunkt[strecke])
+        print(f"Laufe Strecke {strecke}")
 
-    #Prüfen welche Taste gedrückt wurde
-    marsmensch['x'],marsmensch['y'],marsmensch['Seite'],run=marsi.check_key()
-    #Text auswählen, der angezeigt werden soll
-    text2show,xt,yt=check_text(marsmensch['x'],marsmensch['y'])
-    #Bild neu aufbauen
-    xs,ys=sf.redrawGameWindow(text2show,xt,yt,marsmensch,moorhuhn)
+    # Zusätzlich Tastatursteuerung für Marsi (nur erster Marsmensch)
+    akt_mars['x'], akt_mars['y'], akt_mars['Seite'], run = akt_mars['obj'].check_key()
 
+    # Koordinaten im Dict aktuell halten
+    akt_mars['obj'].x = akt_mars['x']
+    akt_mars['obj'].y = akt_mars['y']
+
+    # Moorhühner bewegen
+    for mh in moorhuehner:
+        # Referenz auf Marsi für Kollision
+        mars_dict = {
+            "x": akt_mars['x'],
+            "y": akt_mars['y']
+        }
+
+        if mh['counter'] > mh['intervall']:
+            # Neues Moorhuhn starten
+            mh['y'] = random.randint(10, 300)
+            mh['richtung'] = random.choice(("right", "left"))
+            mh['x'] = 0 if mh['richtung'] == "right" else 1000
+            mh['counter'] = 0
+            mh['Status'] = "Go"
+
+        elif mh['Status'] == "Go":
+            # In welche Richtung soll es gehen
+            if mh['richtung'] == "right":
+                mh['x'], mh['Seite'] = mh['obj'].go_right(mh['x'], 3)
+            else:
+                mh['x'], mh['Seite'] = mh['obj'].go_left(mh['x'], 3)
+
+            # Ist das Moorhuhn in der Nähe der Sonne?
+            if kollidiert_rect({"x": mh['x'], "y": mh['y']},
+                               {"x": sonne_dict['x'], "y": sonne_dict['y']},
+                               width_a=40, height_a=40,
+                               width_b=40, height_b=40):
+                print(
+                    f"{mh['name']} von Sonne verbrannt, xh:{mh['x']}, yh:{mh['y']}")
+                mh['Status'] = 'tot'
+
+            # Ist das Moorhuhn in der Nähe eines Marsmenschen?
+            if kollidiert_rect({"x": mh['x'], "y": mh['y']},
+                               mars_dict,
+                               width_a=40, height_a=40,
+                               width_b=40, height_b=60):
+                print(
+                    f"{mh['name']} von Marsi gefressen, xh:{mh['x']}, xm:{mars_dict['x']}, yh:{mh['y']}, ym:{mars_dict['y']}")
+                mh['Status'] = 'Stop'
+                mh['counter'] = 0
+
+            # Kollision mit anderen Moorhühnern (abstürzen lassen)
+            for mh2 in moorhuehner:
+                if mh2 is mh:
+                    continue
+                if mh2['Status'] == "Go":
+                    if kollidiert_rect(
+                        {"x": mh['x'], "y": mh['y']},
+                        {"x": mh2['x'], "y": mh2['y']},
+                        width_a=40, height_a=40,
+                        width_b=40, height_b=40
+                    ):
+                        print(
+                            f"{mh['name']} und {mh2['name']} kollidieren und stürzen ab!")
+                        mh['Status'] = 'tot'
+                        mh2['Status'] = 'tot'
+
+            # Ist das Moorhuhn am Bildende?
+            if mh['x'] < 0 or mh['x'] > 999:
+                mh['counter'] = 0
+                mh['Status'] = 'Stop'
+
+        elif mh['Status'] == "tot":
+            mh['y'] = mh['obj'].go_down(mh['y'], 2)
+            if mh['y'] >= 340:
+                mh['counter'] = 0
+                mh['Status'] = 'Stop'
+        else:
+            # Warten bis zum nächsten Start
+            mh['counter'] += 1
+
+    # Text auswählen, der angezeigt werden soll (für akt_mars)
+    text2show, xt, yt = check_text(akt_mars['x'], akt_mars['y'])
+
+    # Bild neu aufbauen, inkl. Sonne und aller Figuren
+    # xs, ys zurückgeben und im sonne_dict aktualisieren
+    xs, ys = sf.redrawGameWindow(
+        text2show,
+        xt,
+        yt,
+        marsmenschen=marsmenschen,
+        moorhuehner=moorhuehner
+    )
+    sonne_dict['x'], sonne_dict['y'] = xs, ys
--- stadtlauf_bern_oo_moorhuhn_modul.py
+++ stadtlauf_bern_oo_moorhuhn_modul.py
@@ -1,220 +1,271 @@
 ##############################################
 #
-# Name: stadtlauf_bern_oo_modul.py
+# Name: stadtlauf_bern_oo_moorhuhn_modul.py
 #
 # Author: Peter Christen
 #
-# Version: 1.0
+# Version: 2.0
 #
-# Date: 10.09.2022
+# Date: 10.09.2022 V1.0
+#       29.03.2026 V2.0 Mit KI Überarbeitet
 #
-# Purpose: Modul zu Script Stadtlauf_Bern_OO.py
+# Purpose: Modul zu Script Stadtlauf_Bern_OO_Moorhuhn.py
 #
 ##############################################
 
-#Module
 import pygame
 import datetime
 
-#Initialisierung
+# Initialisierung
 pygame.init()
 pygame.display.set_caption("Spaziergang durch Bern")
-screen = pygame.display.set_mode((1050,400))
+screen = pygame.display.set_mode((1050, 400))
 
-#Bilder
-walkRight = [pygame.image.load('Bilder/R1.png'), pygame.image.load('Bilder/R2.png'), pygame.image.load('Bilder/R3.png'), pygame.image.load('Bilder/R4.png'), pygame.image.load('Bilder/R5.png'), pygame.image.load('Bilder/R6.png'), pygame.image.load('Bilder/R7.png'), pygame.image.load('Bilder/R8.png'), pygame.image.load('Bilder/R9.png')]
-walkLeft = [pygame.image.load('Bilder/L1.png'), pygame.image.load('Bilder/L2.png'), pygame.image.load('Bilder/L3.png'), pygame.image.load('Bilder/L4.png'), pygame.image.load('Bilder/L5.png'), pygame.image.load('Bilder/L6.png'), pygame.image.load('Bilder/L7.png'), pygame.image.load('Bilder/L8.png'), pygame.image.load('Bilder/L9.png')]
-huhnLeft = [pygame.image.load('Bilder/Lchicken1.png'), pygame.image.load('Bilder/Lchicken2.png'), pygame.image.load('Bilder/Lchicken3.png'), pygame.image.load('Bilder/Lchicken4.png'), pygame.image.load('Bilder/Lchicken5.png'), pygame.image.load('Bilder/Lchicken6.png'), pygame.image.load('Bilder/Lchicken7.png'), pygame.image.load('Bilder/Lchicken8.png'), pygame.image.load('Bilder/Lchicken9.png')]
-huhnRight = [pygame.image.load('Bilder/Rchicken1.png'), pygame.image.load('Bilder/Rchicken2.png'), pygame.image.load('Bilder/Rchicken3.png'), pygame.image.load('Bilder/Rchicken4.png'), pygame.image.load('Bilder/Rchicken5.png'), pygame.image.load('Bilder/Rchicken6.png'), pygame.image.load('Bilder/Rchicken7.png'), pygame.image.load('Bilder/Rchicken8.png'), pygame.image.load('Bilder/Rchicken9.png')]
-huhntot = [pygame.image.load('Bilder/chickendead1.png'), pygame.image.load('Bilder/chickendead2.png'), pygame.image.load('Bilder/chickendead3.png'), pygame.image.load('Bilder/chickendead4.png'), pygame.image.load('Bilder/chickendead5.png'), pygame.image.load('Bilder/chickendead6.png'), pygame.image.load('Bilder/chickendead7.png'), pygame.image.load('Bilder/chickendead8.png'), pygame.image.load('Bilder/chickendead8.png')]
+# Bilder
+walkRight = [
+    pygame.image.load('Bilder/R1.png'), pygame.image.load('Bilder/R2.png'),
+    pygame.image.load('Bilder/R3.png'), pygame.image.load('Bilder/R4.png'),
+    pygame.image.load('Bilder/R5.png'), pygame.image.load('Bilder/R6.png'),
+    pygame.image.load('Bilder/R7.png'), pygame.image.load('Bilder/R8.png'),
+    pygame.image.load('Bilder/R9.png')
+]
+walkLeft = [
+    pygame.image.load('Bilder/L1.png'), pygame.image.load('Bilder/L2.png'),
+    pygame.image.load('Bilder/L3.png'), pygame.image.load('Bilder/L4.png'),
+    pygame.image.load('Bilder/L5.png'), pygame.image.load('Bilder/L6.png'),
+    pygame.image.load('Bilder/L7.png'), pygame.image.load('Bilder/L8.png'),
+    pygame.image.load('Bilder/L9.png')
+]
+huhnLeft = [
+    pygame.image.load(
+        'Bilder/Lchicken1.png'), pygame.image.load('Bilder/Lchicken2.png'),
+    pygame.image.load(
+        'Bilder/Lchicken3.png'), pygame.image.load('Bilder/Lchicken4.png'),
+    pygame.image.load(
+        'Bilder/Lchicken5.png'), pygame.image.load('Bilder/Lchicken6.png'),
+    pygame.image.load(
+        'Bilder/Lchicken7.png'), pygame.image.load('Bilder/Lchicken8.png'),
+    pygame.image.load('Bilder/Lchicken9.png')
+]
+huhnRight = [
+    pygame.image.load(
+        'Bilder/Rchicken1.png'), pygame.image.load('Bilder/Rchicken2.png'),
+    pygame.image.load(
+        'Bilder/Rchicken3.png'), pygame.image.load('Bilder/Rchicken4.png'),
+    pygame.image.load(
+        'Bilder/Rchicken5.png'), pygame.image.load('Bilder/Rchicken6.png'),
+    pygame.image.load(
+        'Bilder/Rchicken7.png'), pygame.image.load('Bilder/Rchicken8.png'),
+    pygame.image.load('Bilder/Rchicken9.png')
+]
+huhntot = [
+    pygame.image.load(
+        'Bilder/chickendead1.png'), pygame.image.load('Bilder/chickendead2.png'),
+    pygame.image.load(
+        'Bilder/chickendead3.png'), pygame.image.load('Bilder/chickendead4.png'),
+    pygame.image.load(
+        'Bilder/chickendead5.png'), pygame.image.load('Bilder/chickendead6.png'),
+    pygame.image.load(
+        'Bilder/chickendead7.png'), pygame.image.load('Bilder/chickendead8.png'),
+    pygame.image.load('Bilder/chickendead8.png')
+]
 
 bg = pygame.image.load('Bilder/karte-bern_kl.jpg')
 char = pygame.image.load('Bilder/standing.png')
 sonne = pygame.image.load('Bilder/Sonne2.png')
 
-#Variablen
+# Variablen
 font = pygame.font.SysFont('Comic Sans MS', 20)
 clock = pygame.time.Clock()
-walkCount = 0
-flightCount = 0
-mit_sonnenstand=True
+walkCount = 0        # globaler Animationszähler für Marsmenschen
+mit_sonnenstand = True
 
-keys = pygame.key.get_pressed()
 
-def redrawGameWindow(text2show,xt,yt,marsmensch,moorhuhn):
-      '''Baut das Bild neu auf'''
+def redrawGameWindow(text2show, xt, yt, marsmenschen, moorhuehner):
+    """Baut das Bild neu auf."""
+    global screen, walkCount
 
-      global screen,walkCount,flightCount
+    textimg = font.render(text2show, True, (255, 0, 0))
+    screen.blit(bg, (0, 0))
+    screen.blit(textimg, (xt, yt))
 
-      textimg = font.render(text2show, True, (255, 0, 0))
-      screen.blit(bg, (0,0))  
-      screen.blit(textimg, (xt,yt))
+    # Animation-Counter für Marsis begrenzen
+    if walkCount >= len(walkRight) * 3:
+        walkCount = 0
 
-      if walkCount + 1 >= 27:
-         walkCount = 0
-      if flightCount + 1 >= 27:
-         flightCount = 0
-
-      if marsmensch["Status"]=="Go":
-         if marsmensch['Seite']=='left':  
-            screen.blit(walkLeft[walkCount//3], (marsmensch['x'],marsmensch['y']))
-            walkCount += 1                          
-         elif marsmensch['Seite']=='right':
-            screen.blit(walkRight[walkCount//3], (marsmensch['x'],marsmensch['y']))
-            walkCount += 1
-         else:
-            screen.blit(char, (marsmensch['x'],marsmensch['y']))
+    # Alle Marsmenschen zeichnen
+    for m in marsmenschen:
+        if m["Status"] == "Go":
+            if m['Seite'] == 'left':
+                screen.blit(walkLeft[walkCount // 3], (m['x'], m['y']))
+                walkCount += 1
+            elif m['Seite'] == 'right':
+                screen.blit(walkRight[walkCount // 3], (m['x'], m['y']))
+                walkCount += 1
+            else:
+                screen.blit(char, (m['x'], m['y']))
+                walkCount = 0
+        else:
+            screen.blit(char, (m['x'], m['y']))
             walkCount = 0
-      else:
-         screen.blit(char, (marsmensch['x'],marsmensch['y']))
-         walkCount = 0
-        
-      if moorhuhn["Status"]=='Go':
-         if moorhuhn["Seite"]=='left':  
-            screen.blit(huhnLeft[flightCount//3], (moorhuhn["x"],moorhuhn["y"]))
-            flightCount +=1
-         elif moorhuhn["Seite"]=='right':
-            screen.blit(huhnRight[flightCount//3], (moorhuhn["x"],moorhuhn["y"]))
-            flightCount +=1
-      elif moorhuhn["Status"]=='tot':
-            screen.blit(huhntot[flightCount//3], (moorhuhn["x"],moorhuhn["y"]))
-            flightCount +=1
 
-      if mit_sonnenstand:
-         #Aktueller Sonnenstand
-         xs,ys=sonnenstand()
-         screen.blit(sonne, (xs,ys))
+    # Alle Moorhühner zeichnen – eigener Animationszähler pro Huhn
+    for mh in moorhuehner:
+        # Animationszähler initialisieren, falls noch nicht vorhanden
+        if "anim" not in mh:
+            mh["anim"] = 0
 
-      pygame.display.update() 
+        # Für die verschiedenen Status unterschiedliche Frame-Listen verwenden
+        if mh["Status"] == 'Go':
+            # Begrenzen, damit der Index nie aus der Liste läuft
+            if mh["anim"] >= len(huhnLeft) * 3:
+                mh["anim"] = 0
 
-      return xs,ys
+            if mh["Seite"] == 'left':
+                frame = huhnLeft[mh["anim"] // 3]
+                screen.blit(frame, (mh["x"], mh["y"]))
+                mh["anim"] += 1
+            elif mh["Seite"] == 'right':
+                frame = huhnRight[mh["anim"] // 3]
+                screen.blit(frame, (mh["x"], mh["y"]))
+                mh["anim"] += 1
+
+        elif mh["Status"] == 'tot':
+            if mh["anim"] >= len(huhntot) * 3:
+                mh["anim"] = 0
+            frame = huhntot[mh["anim"] // 3]
+            screen.blit(frame, (mh["x"], mh["y"]))
+            mh["anim"] += 1
+        else:
+            # Status 'Stop' – nichts animieren
+            mh["anim"] = 0
+
+    xs, ys = 0, 0
+    if mit_sonnenstand:
+        xs, ys = sonnenstand()
+        screen.blit(sonne, (xs, ys))
+
+    pygame.display.update()
+    return xs, ys
+
 
 def sonnenstand():
-   '''Errechnet den Sonnenstand anhand der aktuellen Zeit'''
+    """Errechnet den Sonnenstand anhand der aktuellen Zeit."""
+    heute = datetime.datetime.now()
+    stunde = int(heute.strftime("%H"))
+    minute = int(heute.strftime("%M"))
+    x = 1100
+    y = 400
+    if stunde > 6:
+        x = x - (100 * (stunde - 6)) - (minute * 1.66)
+    if 6 < stunde <= 12:
+        y = y - (30 * (stunde - 6)) - (minute * 0.5)
+    else:
+        y = y - (30 * (18 - stunde)) - (minute * 0.5)
 
-   heute = datetime.datetime.now()
-   stunde=heute.strftime("%H")
-   minute=heute.strftime("%M")
-   x=1100
-   y=400
-   if int(stunde) > 6: x=x-(100*(int(stunde)-6))-(int(minute)*1.66)
-   if int(stunde) > 6 and int(stunde) <=12: y=y-(30*(int(stunde)-6))-(int(minute)*0.5)
-   else: y=y-(30*(18-int(stunde)))-(int(minute)*0.5)
+    return int(x), int(y)
 
-   return int(x),int(y)
 
-#Klassen
 class Figur:
-   '''Klasse zum Verwalten der Spielfigur'''
+    """Klasse zum Verwalten der Spielfigur."""
 
-   #Konstruktor Methode
-   def __init__(self,figur):
-      self.figur=figur   #Key Addtribut
-      self.x=20          #Position x
-      self.y=150         #Position y
-      self.max_x=1000    #Max Position x
-      self.max_y=340     #Max Position y
-      self.min_x=-10     #Min Position x
-      self.min_y=-10     #Min Position y
-      self.seite = "Stop"  #Ausgangsposition Figur
+    def __init__(self, figur, width=40, height=60):
+        self.figur = figur      # Name / Key
+        self.x = 20             # Position x
+        self.y = 150            # Position y
+        self.max_x = 1000       # Max Position x
+        self.max_y = 340        # Max Position y
+        self.min_x = -10        # Min Position x
+        self.min_y = -10        # Min Position y
+        self.seite = "Stop"     # Ausgangsposition Figur
+        self.width = width
+        self.height = height
 
-   def go_walk_right(self,x,y):
-      '''Läuft nach einem vorgegebenen Plan nach rechts'''
+    def go_walk_right(self, x, y):
+        """Läuft nach einem vorgegebenen Plan nach rechts."""
+        self.x, self.y = x, y
+        self.seite = "right"
+        return self.x, self.y, self.seite
 
-      self.x,self.y=x,y
-      self.seite = "right"
+    def go_walk_left(self, x, y):
+        """Läuft nach einem vorgegebenen Plan nach links."""
+        self.x, self.y = x, y
+        self.seite = "left"
+        return self.x, self.y, self.seite
 
-      return self.x,self.y,self.seite
+    def go_left(self, x, steps=1):
+        """Nach links gehen."""
+        self.x = x
+        if self.x > self.min_x:
+            self.x -= steps
+            self.seite = "left"
+        else:
+            self.go_stop()
+        return self.x, self.seite
 
-   def go_walk_left(self,x,y):
-      '''Läuft nach einem vorgegebenen Plan nach links'''
+    def go_right(self, x, steps=1):
+        """Nach rechts gehen."""
+        self.x = x
+        if self.x < self.max_x:
+            self.x += steps
+            self.seite = "right"
+        else:
+            self.go_stop()
+        return self.x, self.seite
 
-      self.x,self.y=x,y
-      self.seite = "left"
+    def go_stop(self):
+        """Bewegung stoppen."""
+        self.seite = "stop"
+        global walkCount
+        walkCount = 0
+        return walkCount, self.seite
 
-      return self.x,self.y,self.seite
+    def go_up(self, y, steps=1):
+        """Nach oben gehen."""
+        self.y = y
+        if self.y > self.min_y:
+            self.y -= steps
+        else:
+            self.go_stop()
+        return self.y
 
-   def go_left(self,x,steps=1):
-      '''Nach links gehen'''
+    def go_down(self, y, steps=1):
+        """Nach unten gehen."""
+        self.y = y
+        if self.y < self.max_y:
+            self.y += steps
+        else:
+            self.go_stop()
+        return self.y
 
-      self.x=x
-      if self.x > self.min_x:
-         self.x -= steps
-         self.seite = "left"
-         return self.x,self.seite
-      else:
-         self.go_stop()
+    def rect(self):
+        """Gibt ein pygame.Rect der Figur zurück."""
+        return pygame.Rect(self.x, self.y, self.width, self.height)
 
-   def go_right(self,x,steps=1):
-      '''Nach rechts gehen'''
-     
-      self.x=x
-      if self.x < self.max_x:
-         self.x += steps
-         self.seite = "right"
-         return self.x,self.seite
-      else:
-         self.go_stop()
+    def collides_with(self, other):
+        """Kollision mit anderer Figur basierend auf Rect."""
+        return self.rect().colliderect(other.rect())
 
-   def go_stop(self):
-      '''Bewegung stoppen'''
+    def check_key(self):
+        """Prüfen welche Taste gedrückt wurde."""
+        run = True
 
-      self.seite = "stop"
-      walkCount = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-      return walkCount,self.seite
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.go_right(self.x, 1)
+        elif keys[pygame.K_LEFT]:
+            self.go_left(self.x, 1)
 
-   def go_up(self,y,steps=1):
-      '''Nach oben gehen'''
+        if keys[pygame.K_UP]:
+            self.go_up(self.y, 1)
+        elif keys[pygame.K_DOWN]:
+            self.go_down(self.y, 1)
 
-      self.y=y
-      if self.y > self.min_y:
-         self.y-=steps
-         return self.y
-      else:
-         self.go_stop()
-
-   def go_down(self,y,steps=1):
-      '''Nach unten gehen'''
-
-      self.y=y
-      if self.y < self.max_y:
-         self.y+=steps
-         return self.y
-      else:
-         self.go_stop()
-
-   def __eq__(self, other):
-      if self.x <= other.x+10 and self.x >= other.x-10 and self.y <= other.y+15 and self.y >= other.y-60:
-         return True
-      else:
-        return False
-
-   def check_key(self):
-      '''Prüfen welche Taste gedrückt wurde'''
-
-      for event in pygame.event.get():
-         if event.type == pygame.QUIT:
+        if keys[pygame.K_q]:
             run = False
 
-      keys = pygame.key.get_pressed()
-      if keys[pygame.K_RIGHT]:
-         self.go_right(self.x,1)
-         print("Position x,y: ",self.x,self.y,self.seite)
-      elif keys[pygame.K_LEFT]:
-         self.go_left(self.x,1)
-         print("Position x,y: ",self.x,self.y,self.seite)
-
-      if keys[pygame.K_UP]:
-         self.go_up(self.y,1)
-         print("Position x,y: ",self.x,self.y,self.seite)
-      elif keys[pygame.K_DOWN]:
-         self.go_down(self.y,1)
-         print("Position x,y: ",self.x,self.y,self.seite)
-
-      if keys[pygame.K_q]: 
-         return self.x,self.y,self.seite,False
-      else: 
-         return self.x,self.y,self.seite,True
-       
-
+        return self.x, self.y, self.seite, run
